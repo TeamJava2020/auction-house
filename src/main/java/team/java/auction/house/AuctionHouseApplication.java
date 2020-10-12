4b95182eba9c9271c708d86cdebf25a5cb2ed9a2
TeamJava2020 <test.aplikacja2020@gmail.com>
2020-10-12 21:59:29 +0200

    first commit

---
+++ src/main/java/team/java/auction/house/AuctionHouseApplication.java
@@ -0,0 +1,12 @@
+package team.java.auction.house;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionHouseApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AuctionHouseApplication.class, args);
+        System.out.println("Hello World!");
+    }
+}
