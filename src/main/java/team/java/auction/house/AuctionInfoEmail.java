3b0aec458ea2b0c3dfdd180ad41ee5a923d231a5
maksymilian <maksymilian.kaczmarek@gmail.com>
2020-10-14 21:55:03 +0000

    Added two classes, AuctionInfo with all info about auction and AuctionInfoEmail with all the info that user is gonna get with email

---
+++ src/main/java/team/java/auction/house/AuctionInfoEmail.java
@@ -0,0 +1,12 @@
+package team.java.auction.house;
+
+
+import java.time.LocalDateTime;
+
+
+public class AuctionInfoEmail {
+    private String auctionTitle;
+    private float highestBid;
+    private float userBid;
+    private LocalDateTime endOfTheAuction;
+}
