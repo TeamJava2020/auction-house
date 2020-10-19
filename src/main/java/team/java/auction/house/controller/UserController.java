fba482fe9108f5a26d825b98b3cd276b76e95f8d
maksymilian <maksymilian.kaczmarek@gmail.com>
2020-10-19 22:46:43 +0000

    Fixed UserController issue

--- src/main/java/team/java/auction/house/controller/UserController.java
+++ src/main/java/team/java/auction/house/controller/UserController.java
@@ -39,7 +39,7 @@ public class UserController {
         Optional<UserEntity> user = userService.findUserByLogin(userDto.getLogin());
         if (user.isPresent()) {
             String date = LocalDateTime.now().format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM));
-            return (user.get().getPassword().equals(userDto.getPassword())) ? createOkResponse(userMapper.mapToUserDto(user.get())) //
+            return (user.get().getPassword().equals(userDto.getPassword())) ? createOkResponse(userMapper.toDto(user.get())) //
                     : createErrorResponse(new ErrorMessage(401, "Podane hasło jest nieprawidłowe!", date));
         }
         return createNoDataResponse();
@@ -52,7 +52,7 @@ public class UserController {
             String date = LocalDateTime.now().format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM));
             return createErrorResponse(new ErrorMessage(409, "Podany login już istnieje!", date));
         }
-        userService.saveUser(userMapper.mapToUser(userDto));
+        userService.saveUser(userMapper.toEntity(userDto));
         return createOkResponse();
     }
 
